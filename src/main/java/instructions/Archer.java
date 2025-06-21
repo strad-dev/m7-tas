e93aa6348f4db6cbf54be396eceb647f75c17a11
strad-dev <bqiang3.14@gmail.com>
2025-06-21 11:13:13 -0700

    first attempt at watcher

--- src/main/java/instructions/Archer.java
+++ src/main/java/instructions/Archer.java
@@ -14,69 +14,88 @@ import java.util.Objects;
 
 public class Archer {
 	private static Player archer;
-	@SuppressWarnings("FieldCanBeLocal")
 	private static World world;
 
 	public static void archerInstructions(Player p) {
 		archer = p;
 		world = archer.getWorld();
 		archer.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, -1, 1));
+		Objects.requireNonNull(archer.getInventory().getItem(4)).addUnsafeEnchantment(Enchantment.POWER, 7);
 		Actions.simulateAOTV(archer, new Location(world, -118.5, 70, -202.5));
-		Actions.setFakePlayerHotbarSlot(archer, 4);
+		Actions.setFakePlayerHotbarSlot(archer, 1);
 		Utils.scheduleTask(() -> Actions.move(archer, new Vector(0, 0, 1.12242), 5), 160);
 		Utils.scheduleTask(Archer::clear, 162);
 	}
 
 	public static void clear() {
+		// look at 9.3 yaw after clear
 
 	}
 
 	private static void simulateShoot() {
 		Actions.simulateRightClickAir(archer);
+		Location l = archer.getLocation();
+		l.add(l.getDirection());
+		l.setY(l.getY() + 1.62);
+		int power = archer.getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.POWER);
+		int strength;
+		try {
+			strength = Objects.requireNonNull(archer.getPotionEffect(PotionEffectType.STRENGTH)).getAmplifier();
+		} catch(Exception exception) {
+			strength = 0;
+		}
+		double powerBonus;
+		try {
+			powerBonus = power * 0.05;
+			if(power == 7) {
+				powerBonus += 0.05;
+			}
+		} catch(Exception exception) {
+			powerBonus = 0;
+		}
+
+		double strengthBonus;
+		try {
+			strengthBonus = 0.15 + 0.15 * strength;
+		} catch(Exception exception) {
+			strengthBonus = 0;
+		}
+
+		double add = powerBonus + strengthBonus;
 
 		// Duplex Arrow
+		double finalAdd = add;
 		Utils.scheduleTask(() -> {
-			Location l = archer.getLocation();
-			l.add(l.getDirection());
-			l.setY(l.getY() + 1.62);
-
-			double powerBonus;
-			try {
-				int power = archer.getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.POWER);
-				powerBonus = power * 0.05;
-				if(power == 7) {
-					powerBonus += 0.05;
-				}
-			} catch(Exception exception) {
-				powerBonus = 0;
-			}
-
-			double strengthBonus;
-			try {
-				strengthBonus = 0.15 + 0.15 * Objects.requireNonNull(archer.getPotionEffect(PotionEffectType.STRENGTH)).getAmplifier();
-			} catch(Exception exception) {
-				strengthBonus = 0;
-			}
-
-			double add = powerBonus + strengthBonus;
 			Arrow arrow = world.spawnArrow(l, l.getDirection(), 3, 0.1F);
-			arrow.setDamage(0.5 + add);
+			arrow.setDamage(0.5 + finalAdd);
 			arrow.setPierceLevel(4);
 			arrow.setShooter(archer);
 			arrow.setWeapon(archer.getInventory().getItemInMainHand());
 			arrow.addScoreboardTag("TerminatorArrow");
 		}, 3);
 
-		PotionEffect strength = archer.getPotionEffect(PotionEffectType.STRENGTH);
-		if(strength == null) {
-			archer.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 100, 0));
-		} else {
-			if(strength.getAmplifier() < 5) {
-				archer.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 100, strength.getAmplifier() + 1));
-			} else {
-				archer.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH, 100, 5));
-			}
-		}
+		// Archer Bonus Arrows
+		add *= 5;
+
+		double finalAdd1 = add;
+		Utils.scheduleTask(() -> {
+			Arrow arrow = world.spawnArrow(l, l.getDirection(), 3, 0.1F);
+			arrow.setDamage(2.5 + finalAdd1);
+			arrow.setPierceLevel(4);
+			arrow.setShooter(archer);
+			arrow.setWeapon(archer.getInventory().getItemInMainHand());
+			arrow.addScoreboardTag("TerminatorArrow");
+		}, 5);
+
+		double finalAdd2 = add;
+		Utils.scheduleTask(() -> {
+			Arrow arrow = world.spawnArrow(l, l.getDirection(), 3, 0.1F);
+			arrow.setDamage(2.5 + finalAdd2);
+			arrow.setPierceLevel(4);
+			arrow.setShooter(archer);
+			arrow.setWeapon(archer.getInventory().getItemInMainHand());
+			arrow.addScoreboardTag("TerminatorArrow");
+		}, 10);
 	}
 
 	public static Player getArcher() {
--- src/main/java/instructions/bosses/Watcher.java
+++ src/main/java/instructions/bosses/Watcher.java
@@ -2,9 +2,7 @@ package instructions.bosses;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.craftbukkit.v1_21_R3.profile.CraftPlayerProfile;
 import org.bukkit.entity.EntityType;
@@ -13,19 +11,31 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
+import plugin.Utils;
 
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 public class Watcher {
 	@SuppressWarnings("FieldCanBeLocal")
 	private static Zombie watcher;
+	private static World world;
+	private static final Location ORIGINAL_POSITION = new Location(null, -120.5, 72.0, -88.5, -180, 0);
+	private static final List<Location> MOB_SPAWN_LOCATIONS = new ArrayList<>();
+	private static final List<String> MOB_NAMES = List.of("Diamante Giant", "Bonzo", "Meepy_", "Mallyanke", "valej", "Sillygreenj", "a6j3", "Nucleararmadillo", "ItzZeeka", "akc0303", "nil4k", "editqble", "uncheck", "EvilMerlyn", "JennAiel", "Stenoe", "aalatif_", "Deanvm", "Beethoven_");
+	private static final List<String> SPAWN_LINES = List.of("This guy looks like a fighter.", "Hmmm... this one!", "You'll do.", "Go, fight!", "Go and live again!");
+	private static final List<String> KILLED_LINES = List.of("Not bad.", "That one was weak anyway.", "I'm impressed.", "Very nice.", "Aw, I liked that one.");
+	private static int mobCount = 0;
+	private static final Random random = new Random();
 
-	public static void watcherInstructions(World world) {
+	public static void watcherInstructions(World temp) {
+		mobCount = 0;
+		world = temp;
+		ORIGINAL_POSITION.setWorld(world);
 		if(watcher != null) {
 			watcher.remove();
 		}
-		watcher = (Zombie) world.spawnEntity(new Location(world, -120.5, 72.0, -88.5, -180, 0), EntityType.ZOMBIE);
+		watcher = (Zombie) world.spawnEntity(ORIGINAL_POSITION, EntityType.ZOMBIE);
 		watcher.setAI(false);
 		watcher.setSilent(true);
 		watcher.setPersistent(true);
@@ -46,5 +56,115 @@ public class Watcher {
 		watcher.addPotionEffect(new PotionEffect(PotionEffectType.RESISTANCE, -1, 255, false, false));
 		watcher.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, -1, 255, false, false));
 		Objects.requireNonNull(watcher.getAttribute(Attribute.SCALE)).setBaseValue(1.5);
+
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -131.5, 72, -88.5)); // Diamante Giant
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -131.5, 72, -92.5)); // Bonzo
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -131.5, 76, -92.5)); // Meepy_
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -131.5, 76, -88.5)); // Mallyanke
+		/* -------------------- "Let's see how you can handle this" -------------------- */
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -109.5, 72, -88.5)); // valej
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -109.5, 72, -84.5)); // sillygreenj
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -111.5, 72, -77.5)); // a6j3
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -111.5, 76, -77.5)); // Nucleararmadillo
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -111.5, 80, -77.5)); // TankForM7
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -109.5, 80, -84.5)); // baldkc0303
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -109.5, 76, -84.5)); // nil4k
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -109.5, 76, -88.5)); // editqble
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -109.5, 80, -88.5)); // uncheck
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -109.5, 80, -92.5)); // EvilMerlyn
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -109.5, 76, -92.5)); // JennAiel
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -109.5, 72, -92.5)); // Stenoe
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -111.5, 72, -99.5)); // aalatif_
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -111.5, 76, -99.5)); // Deanvm
+		MOB_SPAWN_LOCATIONS.add(new Location(world, -111.5, 80, -99.5)); // Beethoven_
+
+		sendChatMessage("Things feel a little more roomy now, eh?");
+		Utils.scheduleTask(() -> sendChatMessage("I've knocked down those pillars to go for a more... open concept."), 80);
+		Utils.scheduleTask(() -> sendChatMessage("Plus I needed to give my new friends some space to roam..."), 160);
+		Utils.scheduleTask(() -> travelToAndSpawnMob(MOB_SPAWN_LOCATIONS.getFirst(), MOB_NAMES.getFirst()), 240);
+	}
+
+	private static void travelToAndSpawnMob(Location l, String mobName) {
+		Location current = watcher.getLocation();
+		double speed = 0.6; // blocks per tick
+
+		moveEntitySmooth(current, l, speed, () -> {
+			Location headStart = l.clone();
+			Location endLoc = new Location(world, -120.5, 75, -88.5);
+
+			moveEntitySmooth(headStart, endLoc, 0.3, () -> spawnMob(endLoc, mobName));
+
+			sendChatMessage(SPAWN_LINES.get(random.nextInt(5)));
+
+			Utils.scheduleTask(() -> {
+				mobCount++;
+				if(mobCount == 4 || mobCount == 19) {
+					returnToOriginalPosition();
+				} else {
+					travelToAndSpawnMob(MOB_SPAWN_LOCATIONS.get(mobCount), MOB_NAMES.get(mobCount));
+				}
+			}, 10);
+		});
+	}
+
+	private static void moveEntitySmooth(Location start, Location end, double speed, Runnable onComplete) {
+		Vector direction = end.clone().subtract(start).toVector().normalize().multiply(speed);
+		int steps = (int) (start.distance(end) / speed);
+		for(int i = 0; i < steps; i++) {
+			int finalI = i;
+			Utils.scheduleTask(() -> {
+				Location newLoc = start.clone().add(direction.clone().multiply(finalI));
+				watcher.teleport(newLoc);
+				if(finalI == steps - 1 && onComplete != null) {
+					onComplete.run();
+				}
+			}, i);
+		}
+	}
+
+	private static void spawnMob(Location location, String mobName) {
+		Zombie mob = (Zombie) world.spawnEntity(location, EntityType.ZOMBIE);
+		mob.setCustomNameVisible(true);
+		mob.setAI(false);
+		mob.setSilent(true);
+		mob.setAdult();
+		mob.setPersistent(true);
+		mob.setRemoveWhenFarAway(false);
+		Objects.requireNonNull(mob.getAttribute(Attribute.ARMOR)).setBaseValue(-2);
+		if(mobName.equals("Diamante Giant")) {
+			mob.setCustomName(ChatColor.YELLOW + mobName + ChatColor.RESET + ChatColor.RED + " ❤ " + ChatColor.YELLOW + 80 + "/" + 80);
+			Objects.requireNonNull(mob.getAttribute(Attribute.MAX_HEALTH)).setBaseValue(80);
+			mob.setHealth(80);
+			Objects.requireNonNull(mob.getAttribute(Attribute.SCALE)).setBaseValue(16);
+		} else if(mobName.equals("Bonzo")) {
+			mob.setCustomName(ChatColor.YELLOW + mobName + ChatColor.RESET + ChatColor.RED + " ❤ " + ChatColor.YELLOW + 50 + "/" + 50);
+			Objects.requireNonNull(mob.getAttribute(Attribute.MAX_HEALTH)).setBaseValue(50);
+			mob.setHealth(50);
+		} else {
+			mob.setCustomName(ChatColor.YELLOW + mobName + ChatColor.RESET + ChatColor.RED + " ❤ " + ChatColor.YELLOW + 24 + "/" + 24);
+			Objects.requireNonNull(mob.getAttribute(Attribute.MAX_HEALTH)).setBaseValue(24);
+			mob.setHealth(24);
+		}
+
+		if(mobCount == 19) {
+			Utils.scheduleTask(() -> sendChatMessage("You have proven yourself.  You may pass."), 1);
+		} else {
+			Utils.scheduleTask(() -> sendChatMessage(KILLED_LINES.get(random.nextInt(5))), 1);
+		}
+	}
+
+	private static void returnToOriginalPosition() {
+		if(watcher != null && world != null) {
+			if(mobCount != 19) {
+				moveEntitySmooth(watcher.getLocation(), ORIGINAL_POSITION, 0.6, () -> sendChatMessage("Let's see how you can handle this."));
+				Utils.scheduleTask(() -> travelToAndSpawnMob(MOB_SPAWN_LOCATIONS.get(mobCount - 1), MOB_NAMES.get(mobCount - 1)), 40);
+			} else {
+				moveEntitySmooth(watcher.getLocation(), ORIGINAL_POSITION, 0.6, null);
+			}
+		}
+	}
+
+	private static void sendChatMessage(String message) {
+		Bukkit.broadcastMessage(ChatColor.RED + "[BOSS] The Watcher" + ChatColor.WHITE + ": " + message);
 	}
 }
